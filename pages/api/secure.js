0a29f3b5796e50509a167a3ac1fd2e734376ac09
hatim85 <devconnect.hatim@gmail.com>
2024-11-27 18:13:19 +0530

    modified secure.js

--- pages/api/secure.js
+++ pages/api/secure.js
@@ -1,11 +1,9 @@
 import { Buffer } from 'buffer';
 
-const secure = async (req, res) => {
+export default async (req, res) => {
   const projectId = process.env.NEXT_PUBLIC_PINATA_API_KEY;
   const projectSecret = process.env.NEXT_PUBLIC_PINATA_API_SECRET;
   const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
   // console.log("secure.js auth: ",auth)
   res.status(200).json({ data: auth });
 };
-
-export default secure;
