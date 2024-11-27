ab11b4f8d6831bcb13f700bff168ff507dd81586
hatim85 <devconnect.hatim@gmail.com>
2024-11-27 18:03:26 +0530

    Fully changed in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'next',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
+  extends: "next",
   rules: {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off"
